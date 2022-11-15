308c9c5926101ba8eae7020fe875eecbbdeb9b5d
unknown <vivekyerra30@gmail.com>
2022-11-15 09:45:56 +0530

    game control completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React,{useState} from "react";
 import Board from "./components/Board";
-import './styles/root.scss'
-export default () => (
+import './styles/root.scss';
+import {calculateWinner} from "./helpers";
+
+
+const App = () => {
+    
+  const [board,setBoard]= useState(Array(9).fill(null));
+  const [isNext,setIsNext]= useState(false);
+  const winner= calculateWinner(board);
+  const message= winner ? `Winner is ${winner}` : `Next player is ${isNext ? 'X' : 'O'}`;
+  console.log(winner); 
+
+
+
+  const handleSquareClick= position =>{
+    if(board[position] || winner){
+      return;
+    }
+    setBoard(prev =>{
+      return prev.map((square,pos) => {
+        if (pos === position){
+          return isNext ? 'X' : 'O';
+        }
+        return square;
+      });
+    });
+    setIsNext(prev => !prev);
+  };
+
+
+  return(
   <div className="app">
     <h1>TIC TAC TOE</h1>
-    <Board/>
+    <h2>{message}</h2>
+    <Board board={board} handleSquareClick={handleSquareClick}/>
     
   </div>
   
-);
+  );
+};
+
+export default App;
+
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,26 +1,37 @@
-import React from 'react'
-import Square from './Square'
+import React from 'react';
+import Square from './Square';
 
-const Board = () => {
+
+const Board = ({board , handleSquareClick}) => {
+
+  
+  const renderSq = position => {
+    return(
+    <Square value={board[position]}
+    onClick={ () => {handleSquareClick(position)}}
+    />
+    )
+  }
+
   return (
     <div className='board'>
       <div className='board-row'>
-        <Square value={1}/>
-        <Square value={2}/>
-        <Square value={3}/>
+        {renderSq(0)}
+        {renderSq(1)}
+        {renderSq(2)}
       </div>
       <div className='board-row'>
-        <Square value={4}/>
-        <Square value={5}/>
-        <Square value={6}/>
+        {renderSq(3)}
+        {renderSq(4)}
+        {renderSq(5)}
         </div>
       <div className='board-row'>
-        <Square value={7}/>
-        <Square value={8}/>
-        <Square value={9}/>
+        {renderSq(6)}
+        {renderSq(7)}
+        {renderSq(8)}
         </div>
     </div>
   )
 }
 
-export default Board
+export default Board;
